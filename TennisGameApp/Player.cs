55ef5728e5e764b2453f226984f39dc0816df0cf
Angela <angelapanessa@hotmail.it>
2022-09-21 16:18:26 +0200

    Adding class Player.cs and update Program.cs

---
+++ TennisGameApp/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisGameApp
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int levelPlayer { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(string name, string surname, int levelPlayer)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.levelPlayer = levelPlayer;
+        }
+    }
+}
--- TennisGameApp/Program.cs
+++ TennisGameApp/Program.cs
@@ -1,28 +1,39 @@
 ﻿using System;
+using TennisGameApp;
 
 Random random = new Random();
 
 int scorePlayer1 = 0;
 int scorePlayer2 = 0;
 
+Player player1 = new Player();
+Player player2 = new Player();
+
+Console.WriteLine("Enter name's player 1 : ");
+player1.name = Console.ReadLine();
+Console.WriteLine("Enter surname's player 1 : ");
+player1.surname = Console.ReadLine();
+Console.Write("Enter level's player 1 (between 0 and 10) : ");
+player1.levelPlayer = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter name's player 2 : ");
+player2.name = Console.ReadLine();
+Console.WriteLine("Enter surname's player 2 : ");
+player2.surname = Console.ReadLine();
+Console.WriteLine("Enter level's player 2 (between 0 and 10) : ");
+player2.levelPlayer = int.Parse(Console.ReadLine());
+
+
+
 Console.WriteLine("The tennis game starts");
 Console.WriteLine("");
-while ((scorePlayer1 <= 4 && scorePlayer1-scorePlayer2<2)|| (scorePlayer2 <= 4 && scorePlayer2-scorePlayer1<2)) {
-    if (scorePlayer1 >= scorePlayer2 + 2)
-    {
-        Console.WriteLine("Player 1 win!");
-        break;
-    }
-    else if (scorePlayer1 >= scorePlayer2 + 2)
-    {
-        Console.WriteLine("Player 2 win!");
-        break;
-    }
 
+while ((scorePlayer1 < 4 && (scorePlayer1-scorePlayer2 < 2|| scorePlayer2-scorePlayer1<2) ) || (scorePlayer2 < 4 && (scorePlayer2 - scorePlayer1 < 2 || scorePlayer1-scorePlayer2<2)))
+{
     int scoreGamePlayer1 = 0;
-    scoreGamePlayer1 =random.Next(0,4);
+    scoreGamePlayer1 = random.Next(0, 4);
     scorePlayer1 += scoreGamePlayer1;
-    Console.WriteLine($"It's player 1 turn :  ");
+    Console.WriteLine($"It's {player1.name} turn :  ");
     switch (scoreGamePlayer1)
     {
 
@@ -33,19 +44,19 @@ while ((scorePlayer1 <= 4 && scorePlayer1-scorePlayer2<2)|| (scorePlayer2 <= 4 &
             Console.WriteLine("Fifteen points!");
             break;
         case (2):
-            Console.WriteLine("Thirty points!"); 
+            Console.WriteLine("Thirty points!");
             break;
         case (3):
             Console.WriteLine("Forty points!");
             break;
     }
 
-    Console.WriteLine($"Player 1 total point are : {scorePlayer1}");
+    Console.WriteLine($"{player1.name}'s total point are : {scorePlayer1}");
     Console.WriteLine("");
     int scoreGamePlayer2 = 0;
-    scoreGamePlayer2 = random.Next(0,4);
+    scoreGamePlayer2 = random.Next(0, 4);
     scorePlayer2 += scoreGamePlayer2;
-    Console.WriteLine($"It's player 2 turn :  ");
+    Console.WriteLine($"It's {player2.name} turn :  ");
     switch (scoreGamePlayer2)
     {
 
@@ -62,7 +73,7 @@ while ((scorePlayer1 <= 4 && scorePlayer1-scorePlayer2<2)|| (scorePlayer2 <= 4 &
             Console.WriteLine("Forty points!");
             break;
     }
-    Console.WriteLine($"Player 2 total point are : {scorePlayer2}");
+    Console.WriteLine($"{player2.name}'s total point are : {scorePlayer2}");
     Console.WriteLine("");
 
     if (scoreGamePlayer1 == 3 && scoreGamePlayer1 == scoreGamePlayer2)
@@ -80,5 +91,18 @@ while ((scorePlayer1 <= 4 && scorePlayer1-scorePlayer2<2)|| (scorePlayer2 <= 4 &
     }
 }
 
-Console.WriteLine($"Player 1 has {scorePlayer1} point and player 2 has {scorePlayer2} point.");
+if (scorePlayer1 >= scorePlayer2 + 2)
+{
+    Console.WriteLine($"{player1.name} win!");
+}
+else if (scorePlayer2 >= scorePlayer1 + 2)
+{
+    Console.WriteLine($"{player2.name} win!");
+}
+else if (scorePlayer1 == scorePlayer2)
+{
+    Console.WriteLine("Par!");
+}
+
+Console.WriteLine($"{player1.name} has {scorePlayer1} point and {player2.name} has {scorePlayer2} point.");
 
